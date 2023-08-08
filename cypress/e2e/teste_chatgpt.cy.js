ef3ac0fb2ac658d5ef377490b706f7ab3df74d34
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2023-08-08 16:24:01 -0300

    Filtrando termos

--- cypress/e2e/teste_chatgpt.cy.js
+++ cypress/e2e/teste_chatgpt.cy.js
@@ -1,17 +1,11 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 let palavraChave = "chatgpt"
+
+
+let filterTerms = ['ft.', '!', '@', '?', '&', '|', '#', '(', ')', '[', ']', '*', '"', '/', '-', '–', '=', '',
+'DA', 'DE', 'DO', 'NA', 'NO', 'OF', 'THE', 'É', 'O', 'A', 'E', 'IMAGENS', 'DESCRIÇÃO',
+'#01', '#02', '#03', '#04', '#05'];
 
 
 describe('Script de pesquisa e colheta de dados', () => {
@@ -51,7 +45,13 @@ describe('Script de pesquisa e colheta de dados', () => {
           // Condicionais: - (Caso o index 1 da lista, que é o n° de ocorrência, for maior que o de palavraDominante atual, pra mostrar só as com mais ocorrências)
           //               - (Caso não dê pra encontrar a palavra chave na string atual, pra evitar de mostrar a palavra-chave)
           //               - (Caso a string atual não esteja já na lista final, pra evitar repetições)
-          if((countingArr[k][1] > palavraDominante[1]) && (countingArr[k][0].toUpperCase().indexOf(palavraChave.toUpperCase()) == -1) && (!listaFinal.includes(countingArr[k][0]))){
+          if(
+            (countingArr[k][1] > palavraDominante[1]) 
+            && (countingArr[k][0].toUpperCase().indexOf(palavraChave.toUpperCase()) == -1) 
+            && (!listaFinal.includes(countingArr[k][0]))
+            && (!filterTerms.includes(countingArr[k][0].toUpperCase()))
+            ){
+
             palavraDominante = countingArr[k];
 
           }
@@ -105,13 +105,21 @@ describe('Script de pesquisa e colheta de dados', () => {
             // Condicionais: - (Caso o index 1 da lista, que é o n° de ocorrência, for maior que o de palavraDominante atual, pra mostrar só as com mais ocorrências)
             //               - (Caso não dê pra encontrar a palavra chave na string atual, pra evitar de mostrar a palavra-chave)
             //               - (Caso a string atual não esteja já na lista final, pra evitar repetições)
-            if((countingArr[k][1] > palavraDominante[1]) && (countingArr[k][0].toUpperCase().indexOf(palavraChave.toUpperCase()) == -1) && (!listaFinal.includes(countingArr[k][0]))){
+            if(
+              (countingArr[k][1] > palavraDominante[1]) 
+          && (countingArr[k][0].toUpperCase().indexOf(palavraChave.toUpperCase()) == -1) 
+          && (!listaFinal.includes(countingArr[k][0]))
+          && (!filterTerms.includes(countingArr[k][0].toUpperCase()))
+          ){
+              
               palavraDominante = countingArr[k];
   
             }
           }
-          if(palavraDominante[0] != "Erro 01"){ //Evita algum erro de index ou coisa assim, e só da push na lista caso 
+          if(palavraDominante[0] != "Erro 01"){ //Evita algum erro de index ou coisa assim, e só da push na lista caso
+            
             listaFinal.push(palavraDominante[0]); // tenha sido modificada a palavraDominante do default
+
           };
   
           listaFinal.push("{enter}"); // Da um espaço no final só
@@ -123,7 +131,7 @@ describe('Script de pesquisa e colheta de dados', () => {
   
           // A partir daqui já cola tudo que estiver na lista aq de baixo no invertexto
           cy.invertexta("youtube", listaFinal);
-          cy.wait(10000);
+          cy.wait(1000);
           
         })
 
